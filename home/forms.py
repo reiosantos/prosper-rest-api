c6e03be22f17f80b2e96a96f42baafaba8b5cf3b
reiosantos <reiosantos@yahoo.com>
2017-10-25 00:34:06 +0300

    update to create expense model in home application
    Signed-off-by: reiosantos <reiosantos@yahoo.com>

--- home/forms.py
+++ home/forms.py
@@ -1,9 +1,9 @@
 from crispy_forms.bootstrap import AppendedText
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Reset, Submit, ButtonHolder, HTML, Hidden
+from crispy_forms.layout import Layout, Reset, Submit, ButtonHolder, HTML, Hidden, Field
 from django.forms import ModelForm, DateInput
 
-from home.models import ClubDetails
+from home.models import ClubDetails, Expenses
 
 
 class DetailsForm(ModelForm):
@@ -30,4 +30,33 @@ class DetailsForm(ModelForm):
         model = ClubDetails
         widgets = {
             'start_date': DateInput(format='%Y-%m-%d')
+        }
+
+
+class ExpenseForm(ModelForm):
+
+    helper = FormHelper()
+    helper.label_class = 'col-sm-3'
+    helper.field_class = 'col-sm-9'
+    helper.form_show_errors = True
+
+    helper.layout = Layout(
+        AppendedText('date', '<i class="fa fa-calendar"></i>', css_class='input-sm date-input'),
+        Field('particular', 'UGX', css_class='input-sm'),
+        AppendedText('invested', 'UGX', css_class='input-sm'),
+        AppendedText('recouped', 'UGX', css_class='input-sm'),
+        AppendedText('interest', 'UGX', css_class='input-sm'),
+        ButtonHolder(
+            HTML(
+                '<button type="button" class="btn btn-default pull-right" data-dismiss="modal">Close</button>'
+                '<button type="submit" class="btn btn-primary pull-right">Save</button>'
+            ),
+        ),
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Expenses
+        widgets = {
+            'date': DateInput(format='%Y-%m-%d')
         }
