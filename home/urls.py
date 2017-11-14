73fe8f2081db6fec9d296f3815261efc873a6299
reiosantos <reiosantos@yahoo.com>
2017-11-14 21:33:55 +0300

    added url for handling print functionality
    Signed-off-by: reiosantos <reiosantos@yahoo.com>

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,13 @@
 
+from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
-from django.conf.urls import url
-
-from home.views import index, no_script
+from home.views import index, no_script, print_function
 
 urlpatterns = [
 
     url(r'^$', login_required(index), name='home_page'),
     url(r'^no_script/error/cant-load/page/$', login_required(no_script), name='noscript_page'),
+    url(r'^print/(?:(?P<what>\w+))?$', login_required(print_function), name='print_function'),
 
 ]
