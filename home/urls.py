d700f3001552c6391d6ff9f22bf1bfb26eb74a83
reiosantos <reiosantos@yahoo.com>
2017-11-15 15:35:28 +0300

    Changed print function from fuction to class based view
    Signed-off-by: reiosantos <reiosantos@yahoo.com>

--- home/urls.py
+++ home/urls.py
@@ -2,12 +2,12 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
-from home.views import index, no_script, print_function
+from home.views import index, no_script, PrintFunction
 
 urlpatterns = [
 
     url(r'^$', login_required(index), name='home_page'),
     url(r'^no_script/error/cant-load/page/$', login_required(no_script), name='noscript_page'),
-    url(r'^print/(?:(?P<what>\w+))?$', login_required(print_function), name='print_function'),
+    url(r'^print/(?:(?P<what>\w+))?$', login_required(PrintFunction.as_view()), name='print_function'),
 
 ]
